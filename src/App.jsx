5619dc6e6fc52be09564494c20b90d7ce00523b5
haftamudesta <desta451616@hotmail.com>
2023-03-08 01:27:21 +0300

    Add react configuration using vite

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1> Book Store implemented with react redux</h1>
+    </div>
+  )
+}
+
+export default App
