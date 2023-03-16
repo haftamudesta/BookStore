41709e11e8a0cfa97306e1c98fd480548c36600e
haftamudesta <desta451616@hotmail.com>
2023-03-16 16:13:32 +0300

    Modify BookStore

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/index.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
+import reportWebVitals from './reportWebVitals';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Components files
+import SiteNavbar from './components/navbar';
+import Categories from './components/categories';
+import App from './components/App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <SiteNavbar />
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/categories" element={<Categories />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
-)
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
